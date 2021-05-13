23e028b6b26af64112f7935d1aad64e656fd80ad
anjumabbas <khaakpaayali@gmail.com>
2021-05-13 12:19:43 +0100

    API-4968 - Added title encoder with test
    Co-authored-by: Matthew Clark <mattclark-zerogravit@users.noreply.github.com>

---
+++ test/uk/gov/hmrc/integrationcataloguefrontend/controllers/UrlEncodingHelperSpec.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.integrationcataloguefrontend.controllers
+
+import org.scalatest.WordSpec
+import org.scalatest.Matchers
+
+class UrlEncodingHelperSpec extends WordSpec with Matchers with UrlEncodingHelper {
+
+  "encodeTitle" should {
+    "removing leading and trailing spaces" in {
+      encodeTitle("    API1001 Get Data   ") shouldBe "api1001-get-data"
+
+    }
+
+    "lowercase all letters" in {
+      encodeTitle("API 1001 Get Data") shouldBe "api-1001-get-data"
+    }
+
+    "replace non url friendly characters with hyphens" in {
+      encodeTitle("API#1001 Get Data") shouldBe "api-1001-get-data"
+    }
+
+    "replace spaces with hyphens" in {
+      encodeTitle("API 1001 Get  Data") shouldBe "api-1001-get-data"
+    }
+
+    "multiple hyphens replace with single" in {
+      encodeTitle("API----1001----Get---Data---") shouldBe "api-1001-get-data-"
+    }
+
+
+  }
+}
