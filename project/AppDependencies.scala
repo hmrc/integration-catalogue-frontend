903a2db8162301088fda490db452a50f41d5bc9f
Matthew Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2021-05-18 15:30:38 +0100

    API-4990: Updated libraries and fixed tests (#9)
    Co-authored-by: Anjum Abbas <anjumabbas5@users.noreply.github.com>
    
    Co-authored-by: Anjum Abbas <anjumabbas5@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,22 +6,24 @@ import sbt._
 object AppDependencies {
   lazy val scalaCheckVersion = "1.14.0"
   lazy val enumeratumVersion = "1.6.2"
+  lazy val jacksonVersion = "2.12.2"
+  lazy val bootstrapVersion = "5.3.0"
 
 
   val compile = Seq(
-    "uk.gov.hmrc"                       %% "bootstrap-frontend-play-27"     % "3.4.0",
+    "uk.gov.hmrc"                       %% "bootstrap-frontend-play-27"     % bootstrapVersion,
     "uk.gov.hmrc"                       %% "play-frontend-hmrc"             % "0.57.0-play-27",
-    "uk.gov.hmrc"                       %% "play-frontend-govuk"            % "0.69.0-play-27",
-    "com.fasterxml.jackson.core"        % "jackson-annotations"             % "2.12.2",
-    "com.fasterxml.jackson.core"        % "jackson-databind"                % "2.12.2",
-    "com.fasterxml.jackson.dataformat"  % "jackson-dataformat-yaml"         % "2.12.2",
+    "uk.gov.hmrc"                       %% "play-frontend-govuk"            % "0.71.0-play-27",
+    "com.fasterxml.jackson.core"        % "jackson-annotations"             % jacksonVersion,
+    "com.fasterxml.jackson.core"        % "jackson-databind"                % jacksonVersion,
+    "com.fasterxml.jackson.dataformat"  % "jackson-dataformat-yaml"         % jacksonVersion,
     "io.swagger.parser.v3"              % "swagger-parser-v3"               % "2.0.24",
     "com.beachape"                      %% "enumeratum-play-json"           % enumeratumVersion,
     "com.typesafe.play"                 %% "play-json-joda"                 % "2.9.2"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "3.0.0"                 % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % bootstrapVersion        % Test,
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
     "org.jsoup"               %  "jsoup"                    % "1.10.2"                % Test,
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
