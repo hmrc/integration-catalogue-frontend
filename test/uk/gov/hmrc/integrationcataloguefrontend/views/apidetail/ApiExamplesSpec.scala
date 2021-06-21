2930a96552d265d8ba2972e23c9795dd969ec070
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2021-06-21 14:56:29 +0100

    API-5028: added a prefix to example element ids so we can add request and response ids.

--- test/uk/gov/hmrc/integrationcataloguefrontend/views/apidetail/ApiExamplesSpec.scala
+++ test/uk/gov/hmrc/integrationcataloguefrontend/views/apidetail/ApiExamplesSpec.scala
@@ -33,16 +33,16 @@ class ApiExamplesSpec extends CommonViewSpec with ApiTestData {
   "ApiExampleView" should {
 
     "render page with api details" in new Setup {
-
-      val page: Html = apiExamples.render("title", List(Example("example response name 1", "example response body 1"), Example("example response name 2", "example response body 2")))
+      val idPrefix = "idprefix"
+      val page: Html = apiExamples.render("title", List(Example("example response name 1", "example response body 1"), Example("example response name 2", "example response body 2")), idPrefix)
       val document: Document = Jsoup.parse(page.body)
 
 
       document.getElementById("accordion-examples-heading").text() shouldBe "title"
-      document.getElementById("example-name-0").text() shouldBe "example response name 1"
-      document.getElementById("example-body-0").text() shouldBe "example response body 1"      
-      document.getElementById("example-name-1").text() shouldBe "example response name 2"
-      document.getElementById("example-body-1").text() shouldBe "example response body 2"
+      document.getElementById(idPrefix + "-example-name-0").text() shouldBe "example response name 1"
+      document.getElementById(idPrefix + "-example-body-0").text() shouldBe "example response body 1"      
+      document.getElementById(idPrefix + "-example-name-1").text() shouldBe "example response name 2"
+      document.getElementById(idPrefix + "-example-body-1").text() shouldBe "example response body 2"
     }
   }
 }
